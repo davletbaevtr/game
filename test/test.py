d153ac8634755bc70d02f509fe8c661bcfe11342
davletbaevtr <79284231+davletbaevtr@users.noreply.github.com>
2021-05-05 16:20:28 +0300

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import unittest
-from src import clicker
+import clicker
 
 def to_start_position():
     clicker.dollar_now = clicker.dollar.curr_price()
@@ -10,24 +10,24 @@ def to_start_position():
     clicker.user.rps = 0
 
 class ClickerTest(unittest.TestCase):
-    def auto_clicks(self):
-        clicker.user.updateTotalrps(list_of_improvement)
-        self.assertEqual(clicker.score, 0)
+    def test_auto_clicks(self):
+        clicker.user.updateTotalrps(clicker.list_of_improvement)
+        self.assertEqual(clicker.user.score, 0)
         for clicker.improvement in clicker.list_of_improvement:
-            for i in 100:
+            for i in range(100):
                 if clicker.improvement != clicker.dvorec:
                     clicker.improvement.quantity = i
                     clicker.user.updateTotalrps(clicker.list_of_improvement)
                     self.assertEqual(clicker.user.rps, clicker.improvement.base_cost * i)
                     to_start_position()
 
-    def check_cost(self):
+    def test_check_cost(self):
         for clicker.improvement in clicker.list_of_improvement:
-            for i in 100:
+            for i in range(100):
                 if clicker.improvement != clicker.dvorec:
                     clicker.improvement.quantity = i
-                    self.assertEqual(clicker.improvement.getTotalCost(), int(self.base_cost * self.increase_per_purchase**(i)))
+                    self.assertEqual(clicker.improvement.getTotalCost(), int(clicker.improvement.base_cost * clicker.improvement.increase_per_purchase**(i)))
                     to_start_position()
 
-class FastClickerTest(unittest.TestCase):
-    clicker.run()
+if __name__ == '__main__':
+    unittest.main()
