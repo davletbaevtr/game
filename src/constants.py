33d48a932c9190e1e8aab3aea7e2deba0ede1231
davletbaevtr <79284231+davletbaevtr@users.noreply.github.com>
2021-05-11 05:59:12 +0300

    Add files via upload

--- src/constants.py
+++ src/constants.py
@@ -15,8 +15,8 @@ DARK_BLUE = (51, 90, 114)
 
 time_for_req = 0
 
-common_path = 'resources/sprites/'
-background_img = pygame.image.load(common_path + 'background.png')
+common_path = 'sprites/'
+background_img = pygame.image.load(common_path + '/background.png')
 putin_img = pygame.image.load(common_path + 'putin_img.png')
 polic_img = pygame.image.load(common_path + 'police.png')
 rosgv_img = pygame.image.load(common_path + 'rosgv.png')
